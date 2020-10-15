c82837bd61a8b3225680499b5d02e1fd7b1f656a
King LEE <a0988698776@gmail.com>
2020-10-15 23:56:51 +0800

    fetch: check for duplicate Mongoose database

--- routes/modules/result.js
+++ routes/modules/result.js
@@ -10,9 +10,28 @@ router.post('/', (req, res) => {
   console.log(req.get('host')) // local:3000
 
   let shortURL = ''
+
+  /*
+  let shortURL = ''
   shortURL = `${origin}/${generateShortUrl()}`
   console.log('shortUrl:', shortURL)
   res.render('generated', { origin, shortURL })
+  */
+  Url.find()
+    .lean()
+    // 看URL資料庫是否有相同網址,如果沒有產生新的短網址
+    .then(urls => {
+      const checked = urls.some(url => url.origin === origin)
+      if (checked) {
+        const index = urls.findIndex(url => url.origin === origin)
+        console.log(index)
+      } else {
+        shortURL = `${origin}/${generateShortUrl()}`
+        Url.create({ origin, shortURL })
+        res.render('generated', { origin, shortURL })
+      }
+    })
+    .catch(error => console.log(error))
 })
 
 module.exports = router
