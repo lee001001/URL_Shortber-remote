66edc51bec5cfc846f0eec4ed7510781994ff519
King LEE <a0988698776@gmail.com>
2020-10-15 19:36:50 +0800

    feat: add create generate_shorturl.js

---
+++ generate_shorturl.js
@@ -0,0 +1,34 @@
+function picked (array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
+}
+
+function generateShortUrl () {
+  // define the character
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+  const numbers = '0123456789'
+  const allCharacters = [lowerCaseLetters, upperCaseLetters, numbers]
+
+  console.log(allCharacters)
+  // create a collection to store things user picked up
+  let collection = []
+
+  allCharacters.forEach((characters) => {
+    collection = collection.concat(characters.split(''))
+  })
+  console.log('collection', collection)
+
+  // start generating shortUrl
+  let shortUrl = ''
+  for (let i = 0; i < 5; i++) {
+    shortUrl += picked(collection)
+  }
+  console.log(shortUrl)
+
+  // return the short-Url
+  console.log('This function will generate shorted url')
+  return shortUrl
+}
+
+generateShortUrl()
