5cae74705b862e567038cae22e4c6d3ddc5d12c0
King LEE <a0988698776@gmail.com>
2020-10-16 01:04:58 +0800

    chore: heroku init

--- confing/mongoose.js
+++ confing/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/url-shorten-service'
+
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', () => {
